93d1c93278e259fe66610e40fa85772bfeb34e66
Rya Meyvriska <rya.meyvriska@mail.ugm.ac.id>
2017-12-24 18:04:58 +0700

    add deskripsi lowongan (ui-activity)

---
+++ AgriStreetApp/app/src/main/java/id/agristreet/agristreetapp/ui/DeskripsiLowonganActivity.java
@@ -0,0 +1,14 @@
+package id.agristreet.agristreetapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import id.agristreet.agristreetapp.R;
+
+public class DeskripsiLowonganActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deskripsi_lowongan);
+    }
+}
