d142e6909425bcf5414d3245dfea7aea89b56d19
Rya Meyvriska <rya.meyvriska@mail.ugm.ac.id>
2017-12-31 16:25:19 +0700

    add activity addLowongan

---
+++ AgriStreetApp/app/src/main/java/id/agristreet/agristreetapp/ui/AddLowonganActivity.java
@@ -0,0 +1,14 @@
+package id.agristreet.agristreetapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import id.agristreet.agristreetapp.R;
+
+public class AddLowonganActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_lowongan);
+    }
+}
